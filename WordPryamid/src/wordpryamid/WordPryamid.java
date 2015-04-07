46fee785afd5ef35bb3047f0aea4c8d235990112
PoisonedMinds <steven_biro@hotmail.com>
2015-04-07 12:34:15 -0400

    first commit

---
+++ WordPryamid/src/wordpryamid/WordPryamid.java
@@ -0,0 +1,18 @@
+
+
+package wordpryamid;
+import javax.swing.*;
+/**
+ * @title WordPryamid
+ * @author Steven Biro
+ * @teacher Mr. J. Carron
+ * @date 7-Apr-2015 12:34:10 PM
+ * @purpose The purpose of this program is to
+ */
+public class WordPryamid {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
