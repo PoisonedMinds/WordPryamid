402e2fb43e712ed9a39f7b279f4871ab1ee90a90
PoisonedMinds <steven_biro@hotmail.com>
2015-04-08 12:00:45 -0400

    done

--- WordPryamid/src/wordpryamid/WordPryamid.java
+++ WordPryamid/src/wordpryamid/WordPryamid.java
@@ -1,18 +1,35 @@
-
-
 package wordpryamid;
+
 import javax.swing.*;
+
 /**
  * @title WordPryamid
  * @author Steven Biro
  * @teacher Mr. J. Carron
  * @date 7-Apr-2015 12:34:10 PM
- * @purpose The purpose of this program is to
+ * @purpose The purpose of this program is to create a pyramid out of words
  */
 public class WordPryamid {
 
+    static int counter;//setup counter for spaces
+
     public static void main(String[] args) {
-        
+        String word;
+        word = JOptionPane.showInputDialog("Input a word.");//get a word
+        pyramid(word);
+    }
+
+    private static void pyramid(String word) {
+        for (int i = 0; i < counter; i++) {//output spaces
+            System.out.print(" ");
+        }
+        counter++;
+        System.out.println(word);//recursion
+        if (word.length() > 2) {
+            word = word.substring(1, word.length() - 1);//calculate new word
+            pyramid(word);
+        }
+
     }
 
 }
